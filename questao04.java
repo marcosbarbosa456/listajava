ec9609622fff56cfa31955d46503fb2ca9c2e32d
Marcos Barbosa <102072987+marcosbarbosa456@users.noreply.github.com>
2023-04-11 09:43:48 -0300

    att quest 4

--- questao04.java
+++ questao04.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class questao04 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            double raizCubica = Math.cbrt(numero);
+            System.out.printf("A raiz cúbica de %d é %.2f", numero, raizCubica);
+        } else {
+            double raizQuadrada = Math.sqrt(numero);
+            System.out.printf("A raiz quadrada de %d é %.2f", numero, raizQuadrada);
+        }
+
+        scanner.close();
+    }
 }
+
